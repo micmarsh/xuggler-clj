8a17d223301444ac6fca134dd1e0245d2a178ccb
micmarsh <michael.marsh42@gmail.com>
2013-01-27 17:05:20 -0500

    made options optional

--- src/xuggler/core.clj
+++ src/xuggler/core.clj
@@ -2,17 +2,19 @@
 (import [com.xuggle.mediatool
         ToolFactory IMediaReader IMediaWriter ])
 
-(defn convert [from to options]
+(defn convert [from to & options]
     (let [reader (ToolFactory/makeReader from)
           writer (ToolFactory/makeWriter to, reader)
           ;optional adding of debugging output to writer
+          option-map (apply merge (cons {} options))
           debug (fn [writer] (.addListener writer
             (ToolFactory/makeDebugListener )))]
 
+
           (.addListener reader writer)
-          (if (options :debug) (debug writer))
+          (if (option-map :debug) (debug writer))
           (while (= nil (.readPacket reader)))))
 
 (defn -main [& args]
     (let [argsv (vec args)]
-        (convert (argsv 0) (argsv 1) {:debug true})))
+        (convert (argsv 0) (argsv 1) )))
